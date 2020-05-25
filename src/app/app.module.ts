862f2eeec7fa2e75e48096fbcc33128f8a650793
crcastelano <celio.djava@gmail.com>
2020-05-25 11:53:04 -0300

    Criação do projeto. Criação dos componentes básicos. Copiar páginas geradas pelo Mobrise4 para cada componente

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { ListComponent } from './components/list/list.component';
+import { AddFormComponent } from './components/add-form/add-form.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarouselComponent,
+    ListComponent,
+    AddFormComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
